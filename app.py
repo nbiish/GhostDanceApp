e857b97c66cce3c86742bc4a413bc6a0924af399
Nbiish <K3NW48@users.noreply.github.com>
2023-03-18 18:14:07 -0400

    Update app.py
    visual

--- app.py
+++ app.py
@@ -10,17 +10,11 @@ st.write("This is the patent that the math is based on.")
 # Add a link
 st.markdown("https://patents.google.com/patent/US20060014125A1/en")
 
+st.markdown("### Basics from the patent")
+st.write("The calculation is based on your weight that is calculated by the average persons water content at 67%. Because the atoms making up water sit near the `Planck boundary`, we are able to move that mass outside of the boundary.")
 st.write("""* Purpose of the training system is to increase human energy substantially 
-* Result of training will enable capabilities such as:
-    * Walking through walls
-    * Body levitation
-    * Instantaneous healing of infections
-    * Full-body teleportation to another location
-    * Remote viewing at vast distances in terms of light-years
-    * Looking into hyperspace co-dimensions
     
 * Invention based on the relationship between water molecule and boundary between space and hyperspace
-* Mass of water molecule = energy of water molecule at Planck boundary
 * Body composed of 67% water and sits on the boundary
 * Additional increase in energy would move body out of dimension into "hyperspace"
 * Human beings are hyperspace energy beings living in physical bodies
@@ -35,7 +29,8 @@ st.write("""* Purpose of the training system is to increase human energy substan
 unit = st.sidebar.selectbox("Choose a unit of weight", ["pounds", "kilograms"])
 
 # Create a text input field for the user to enter their weight
-weight = st.number_input("Enter your weight:", value=1)
+st.markdown("### Enter your weight below")
+weight = st.number_input("Weight:", value=1)
 
 # Convert the weight to the selected unit
 if unit == "pounds":
@@ -48,8 +43,8 @@ length_in_meters = (50.909563606 / weight_in_kg) * 1
 
 # Convert the length to feet and display the result
 length_in_feet = length_in_meters * 3.28084
-st.write(f"Meters per second to step: {length_in_meters}")
-st.write(f"Feet per second to step: {length_in_feet}")
+st.write(f"Meters per second to step: {length_in_meters:.3f}")
+st.write(f"Feet per second to step: {length_in_feet:.3f}")
 
 st.markdown("""
 ### Use a metronome to improve your accuracy.
